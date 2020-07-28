03bdf7c19f764fe79e763f326f1d2dd3b44a3e73
John Kevin Losito <johnkevinlosito@gmail.com>
2020-07-28 13:06:48 +0800

    Add unit test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -2,8 +2,11 @@ import json
 
 import pytest
 
-from hello_world import app
+from visitor_count import app
+import os
 
+os.environ['AWS_DEFAULT_REGION'] = 'ap-southeast-1'
+os.environ['DynamoDBTable'] = 'visitorCounter'
 
 @pytest.fixture()
 def apigw_event():
@@ -34,7 +37,7 @@ def apigw_event():
             },
             "stage": "prod",
         },
-        "queryStringParameters": {"foo": "bar"},
+        "queryStringParameters": {"siteUrl": "cloudresume.johnkevinlosito.com"},
         "headers": {
             "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
             "Accept-Language": "en-US,en;q=0.8",
@@ -46,7 +49,7 @@ def apigw_event():
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Upgrade-Insecure-Requests": "1",
             "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "Host": "1234567890.execute-api.ap-southeast-1.amazonaws.com",
             "X-Forwarded-Proto": "https",
             "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
             "CloudFront-Is-Tablet-Viewer": "false",
@@ -62,12 +65,11 @@ def apigw_event():
     }
 
 
-def test_lambda_handler(apigw_event, mocker):
-
+def test_lambda_handler(apigw_event):
+    
     ret = app.lambda_handler(apigw_event, "")
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+    assert "visitCount" in ret["body"]
+    assert int(data["visitCount"]) > 0
